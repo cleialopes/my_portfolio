28a3ff2d0a5cf3bca3ccb428bbdfaf14dc77c3cf
cleialopes <cleialopes530@gmail.com>
2025-06-18 18:42:31 +0200

    actualizacion del navegador

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 require('dotenv').config(); // Cargar variables de entorno si usas .env
 
 // Servir archivos estáticos desde la carpeta "public"
 app.use(express.static('public'));
+
+// Middleware para aceptar URLs como "/index" en vez de "/index.html"
+app.use((req, res, next) => {
+    if (!path.extname(req.path)) {
+        const filePath = path.join(__dirname, 'public', req.path + '.html');
+        if (fs.existsSync(filePath)) {
+            return res.sendFile(filePath);
+        }
+    }
+    next();
+});
 
 // Manejar rutas desconocidas redirigiendo al index.html (SPA soporte)
 app.get('*', (req, res) => {
